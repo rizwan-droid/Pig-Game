cd240922929565574a7ce840af81bfdb8049adbe
Rizwan Ahamad <ahamadrizwan320@gmail.com>
2024-03-27 12:53:57 +0530

    changed the winning score to 100 and textcontent winner instead of the player name

--- script.js
+++ script.js
@@ -77,11 +77,11 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
-
+      document.querySelector('#name--' + activePlayer).textContent = 'winner!'
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
